ef1ffb17974db34b3efd38fbcc8fbb15912e899c
Z_TECHNOLOGY <theyorulmazz42@gmail.com>
2023-04-23 00:12:26 +0300

    Update

--- app/src/main/java/com/theyorulmazz/myrestaurant/Activity/MainActivity.java
+++ app/src/main/java/com/theyorulmazz/myrestaurant/Activity/MainActivity.java
@@ -2,13 +2,22 @@ package com.theyorulmazz.myrestaurant.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.theyorulmazz.myrestaurant.Adaptor.CategoryAdaptor;
+import com.theyorulmazz.myrestaurant.Adaptor.PopularAdaptor;
+import com.theyorulmazz.myrestaurant.Domain.CategoryDomain;
+import com.theyorulmazz.myrestaurant.Domain.FoodDomain;
 import com.theyorulmazz.myrestaurant.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+private RecyclerView.Adapter adapter,adapter2;
+private RecyclerView recyclerViewCategoryList,recyclerViewPopularList;
     @Override
     protected void onCreate(Bundle savedInstanceState){
 
@@ -16,5 +25,37 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        recyclerViewCategory();
+        recyclerViewPopular();
+    }
+
+    private void recyclerViewCategory(){
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategoryList=findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category=new ArrayList<>();
+        category.add(new CategoryDomain("Pizza","cat_1"));
+        category.add(new CategoryDomain("Burger","cat_2"));
+        category.add(new CategoryDomain("Sandviç","cat_3"));
+        category.add(new CategoryDomain("İçecekler","cat_4"));
+        category.add(new CategoryDomain("Tatlı","cat_5"));
+
+        adapter=new CategoryAdaptor(category);
+        recyclerViewCategoryList.setAdapter(adapter);
+    }
+    private void recyclerViewPopular(){
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewPopularList=findViewById(R.id.recyclerView2);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<FoodDomain> foodList=new ArrayList<>();
+        foodList.add(new FoodDomain("Pepperoni Pizza","pop_1","slices pepperoni,mozzarella cheese, fresh oregano, ground black pepper,pizza sauce",9.76));
+        foodList.add(new FoodDomain("Cheese Burger","pop_2","beef, Gouda Cheese, Special Sauce, Lettuce, tomato",8.79));
+        foodList.add(new FoodDomain("Vegetable Pizza","pop_3","olive oil, Vegetable oil, pitted kalamata, cherry tomatoes, fresh oregano, basil",8.5));
+
+        adapter2=new PopularAdaptor(foodList);
+        recyclerViewPopularList.setAdapter(adapter2);
     }
 }
